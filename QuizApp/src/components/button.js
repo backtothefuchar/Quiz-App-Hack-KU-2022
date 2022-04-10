9a54d0390355eb5cf994cf6315426079c2671a53
backtothefuchar <wincharans@gmail.com>
2022-04-09 23:48:12 -0500

    add button component

---
+++ QuizApp/src/components/button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const CustomButton = ({text}) => {
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.texty}>{text}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+    },
+    button: {
+        backgroundColor: "#DB5EAD",
+        borderRadius: 10,
+        height: 50,
+        margin: 10,
+        padding: 5,
+    },
+    texty: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginVertical: 6,
+    },
+});
+
+export default CustomButton;
