47731d54a6a0af9f3ced52a91f17ff70ee6eb7b8
backtothefuchar <wincharans@gmail.com>
2022-04-10 02:01:23 -0500

    add question/answer API and functionality

--- QuizApp/src/screens/quiz/quiz.js
+++ QuizApp/src/screens/quiz/quiz.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import CustomButton from "../../components/button";
 import { useNavigation } from "@react-navigation/native";
 
+
 const Quiz = () => {
     const navigation = useNavigation();
     
+    const [questions, setQuestions] = useState();
+    const [ques, setQues] = useState(0);
+
+    const getQuiz = async () => {
+        const url = "https://opentdb.com/api.php?amount=10&type=multiple&encode=url3986";
+        const response = await fetch(url);
+        const data = await response.json();
+        setQuestions(data.results);
+    };
+
+    useEffect(() => {
+        getQuiz()
+    }, []);
+
+    const handleNext = () => {
+        setQues(ques + 1);
+    };
+
     return (
         <View style={styles.container}>
 
+        {questions && (   
+            <View>
+
             <View style={styles.questionContainer}>
-                <Text style={styles.text}>Quiz lol imagine this is a question</Text>
+                <Text style={styles.text}>{decodeURIComponent(questions[ques].question)}</Text>
             </View>
 
             <View style={styles.answerContainer}>
 
-                <CustomButton style={styles.text} text="Answer 1" />
+                <CustomButton style={styles.text} text={decodeURIComponent(questions[ques].question)} />
 
                 <CustomButton text="Answer 2" />
 
@@ -31,15 +53,23 @@ const Quiz = () => {
                     <Text style={styles.textButton}>Skip</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                {ques!==9 && <TouchableOpacity style={styles.button} onPress={handleNext}>
                     <Text style={styles.textButton}>Next</Text>
-                </TouchableOpacity>
+                </TouchableOpacity>}
 
+                {ques===9 && <TouchableOpacity style={styles.button} onPress={()=>null}>
+                    <Text style={styles.textButton}>Results</Text>
+                </TouchableOpacity>}
+
+                {/*
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Result")}>
                     <Text style={styles.textButton}>End</Text>
                 </TouchableOpacity>
+                */}
 
             </View>
+            </View>
+        )}
         </View>
     );
 };
@@ -50,15 +80,19 @@ const styles = StyleSheet.create({
         backgroundColor: '#111d52',
     },
     text: {
-        marginTop: 30,
+        marginTop: 60,
+        marginBottom: 30,
+        marginHorizontal: 20,
         color: "white",
-        fontSize: 30,
+        fontSize: 20,
     },
     questionContainer: {
         alignItems: "center",
         justifyContent: "center",
+        height: 200,
     },
     answerContainer: {
+        marginHorizontal: 20,
     },
     button: {
         backgroundColor: "#1f2f78",
@@ -70,7 +104,7 @@ const styles = StyleSheet.create({
     bottomContainer: {
         justifyContent: "space-between",
         flexDirection: "row",
-        marginTop: '90%',
+        marginTop: '70%',
         marginHorizontal: 30,
     },
     textButton: {
