7bd341054b79729f58338ba65265ded89b1c95f7
backtothefuchar <wincharans@gmail.com>
2022-04-09 22:27:40 -0500

    add result sheet

---
+++ QuizApp/src/screens/result/result.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Result = () => {
+    return (
+        <View style={styles.container}>
+            <Text>Home lol</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+export default Result;
